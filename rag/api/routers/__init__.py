79fe785c9884ddb0f543f601ca206e0f27c48be6
renyh <1365305148@qq.com>
2026-03-11 10:52:14 +0800

    feat: 删除多余代码

--- rag/api/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-路由模块
-"""
-
-from .knowledge_base import router as kb_router
-from .document import router as doc_router
-from .chat import router as chat_router
-
-__all__ = ["kb_router", "doc_router", "chat_router"]
